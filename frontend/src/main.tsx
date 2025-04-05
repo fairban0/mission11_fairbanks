f1fb3107eaa6bf243ff076228e3d172877c78b5d
fairban0 <fairban0@byu.edu>
2025-04-04 19:28:46 -0600

    rebuilding with dist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap-icons/font/bootstrap-icons.css';
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Should make collapse work (only works on mobile mode)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Should make collapse work (only works on mobile mode)
 
 createRoot(
   document.getElementById(
