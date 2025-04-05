bcdca0f4a57c5180fc703bc033562174ea2e64ce
fairban0 <fairban0@byu.edu>
2025-04-04 18:55:11 -0600

    removing strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+//import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
@@ -11,7 +11,7 @@ createRoot(
     'root'
   )!
 ).render(
-  <StrictMode>
+  //<StrictMode>
     <App />
-  </StrictMode>
+  //</StrictMode>
 );
